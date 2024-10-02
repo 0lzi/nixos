518a14195249f1ab8b3e58af9d11598e4f32b649
Oliver Kelly <foolishhim@gmail.com>
2024-10-02 19:09:24 +0100

    update user

--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "oliverkelly";
-  home.homeDirectory = "/home/oliverkelly";
+  home.username = "oli";
+  home.homeDirectory = "/home/oli";
   home.stateVersion = "24.05"; # Please read the comment before changing.
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
