ca90b7ef7f504f813d18fb418f0e08839cfc7495
Oliver Kelly <foolishhim@gmail.com>
2024-10-02 12:46:31 +0100

    renamed hosts

--- hosts/nixos-laptop/flake.nix
+++ hosts/nixos-laptop/flake.nix
@@ -8,7 +8,7 @@
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     # NOTE: 'nixos' is the default hostname set by the installer
-    nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
       # NOTE: Change this to aarch64-linux if you are on ARM
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
