e4e7d34e66ee841675ab4cbcc610e16ed02c3125
Oliver Kelly <foolishhim@gmail.com>
2024-10-25 17:17:33 +0100

    format lua files

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -17,23 +17,23 @@ return {
   {
     "kdheepak/lazygit.nvim",
     lazy = false,
-    dependencies =  {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim"
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
     },
     config = function()
-        require("telescope").load_extension("lazygit")
+      require("telescope").load_extension "lazygit"
     end,
   },
   {
     "mpas/marp-nvim",
     lazy = false,
-    config = function ()
-      require("marp").setup({
+    config = function()
+      require("marp").setup {
         port = 8080,
         wait_for_response_timeout = 30,
         wait_for_response_delay = 1,
-      })
+      }
     end,
   },
   -- These are some examples, uncomment them if you want to see them work!
