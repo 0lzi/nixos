981f6a9568df8a2c61618252596e9e14fbdfdb91
0lzi <foolishhim@gmail.com>
2024-11-29 20:39:30 +0000

    added extra lsp

--- dotfiles/nvim/lua/configs/lspconfig.lua
+++ dotfiles/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,16 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html",
+  "cssls",
+  "bashls",
+  "dockerls",
+  "docker_compose_language_service",
+  "ansiblels",
+  "helm_ls",
+  "terraformls",
+  "yamlls"
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,10 +25,7 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-lspconfig.ansiblels.setup {
-  filetypes = { "yaml", "yml" },
-}
--- configuring single server, example: typescript
+--lconfiguring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
